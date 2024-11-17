c03ded00bcac61d10ea173e767d2ffc98b2b4832
Ananthakrishnan T H <ananthakrishnan.th@gmail.com>
2024-11-17 09:52:13 +0530

    Added mealDashboard components

---
+++ Frontend/src/app/components/user components/meal-dashboard/delete-meal/delete-meal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-meal',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-meal.component.html',
+  styleUrl: './delete-meal.component.css'
+})
+export class DeleteMealComponent {
+
+}
---
+++ Frontend/src/app/components/user components/meal-dashboard/list-meal/list-meal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-meal',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-meal.component.html',
+  styleUrl: './list-meal.component.css'
+})
+export class ListMealComponent {
+
+}
