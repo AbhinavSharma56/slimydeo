1ee55c59e97b3310d5485cbf9a70d39edee29d36
Ananthakrishnan T H <ananthakrishnan.th@gmail.com>
2024-11-20 15:49:00 +0530

    Updated (Display the name in navbar)

--- Frontend/src/app/components/user components/user-navbar/user-navbar.component.ts
+++ Frontend/src/app/components/user components/user-navbar/user-navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { UserProfileService } from '../../../services/user-profile.service';
 
 @Component({
   selector: 'app-user-navbar',
@@ -8,8 +9,37 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './user-navbar.component.html',
   styleUrl: './user-navbar.component.css',
 })
-export class UserNavbarComponent {
+export class UserNavbarComponent implements OnInit {
+  userProfileObj = {
+    username: '',
+    fullName: '',
+    dob: '',
+    email: '',
+    mobileNumber: '',
+    gender: '',
+    address: '',
+    weight: null,
+    height: null,
+  };
+  
   router = inject(Router);
+
+  ngOnInit(): void {
+    const username = localStorage.getItem('loggedUser');
+    if (username) {
+      this.userProfileService.getUserProfile(username).subscribe(
+        (response) => {
+          this.userProfileObj = response.data;
+          console.log(this.userProfileObj);
+        },
+        (error) => {
+          console.error('Error fetching user profile:', error);
+        }
+      );
+    }
+  }
+
+  constructor(private userProfileService: UserProfileService) {}
 
   logout(): void {
     // Clear user data from localStorage or any session management
